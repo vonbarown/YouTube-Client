c2822687a37a10b4c5df299f824de5b76abff0ed
vonbarown <vonielbrown@pursuit.org>
2020-01-19 11:51:31 -0500

    fetching data fromapi endpoint

--- src/Pages/Home/Homepage.jsx
+++ src/Pages/Home/Homepage.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { apiKey } from '../../secrets'
+import axios from 'axios'
 
 class HomePage extends Component {
     constructor() {
@@ -9,7 +11,6 @@ class HomePage extends Component {
         }
     }
 
-
     handleInput = e => {
         const value = e.target.value;
         this.setState({
@@ -18,19 +19,36 @@ class HomePage extends Component {
         });
     };
 
-    handleClick = () => {
-        console.log('yolo');
+    searchVideo = async () => {
+        try {
+            const { data: { items } } = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${this.state.search}&key=${apiKey}`)
+
+            this.setState({
+                results: items
+            })
+
+        } catch (error) {
+            console.log(error);
+
+        }
     }
+
+
 
     render() {
         console.log(this.state);
+        const { results } = this.state
 
         return (
             <div className='homepage'>
                 <form onSubmit={e => e.preventDefault()}>
                     <input type="text" name='search' placeholder='search' onChange={this.handleInput} />
-                    <input type="submit" value='search' onClick={this.handleClick} />
+                    <input type="submit" value='search' onClick={this.searchVideo} />
                 </form>
+
+                {
+                    results.length === 0 ? <div>No Search Results Yet!, Please submit a search above</div> : null
+                }
             </div>
         )
     }
