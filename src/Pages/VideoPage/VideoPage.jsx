6f8d2226f5f496dcc0cacf2e49bd55ca8c010d21
vonbarown <vonielbrown@pursuit.org>
2020-01-19 21:04:59 -0500

    flushed out videopage

--- src/Pages/VideoPage/VideoPage.jsx
+++ src/Pages/VideoPage/VideoPage.jsx
@@ -7,33 +7,66 @@ class VideoPage extends React.Component {
     constructor() {
         super()
         this.state = {
-            videInfo: []
+            videInfo: [],
+            name: '',
+            comment: ''
         }
     }
 
-    loadVideoInfo = async () => {
-        // const
-    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        let { videInfo, name, comment } = this.state;
+        const newItem = { name, comment };
+        this.setState({
+            videInfo: [...videInfo, newItem],
+            name: '',
+            comment: ''
+        });
+    };
+
+    handleInput = e => {
+        const value = e.target.value;
+        this.setState({
+            ...this.prevState,
+            [e.target.name]: value
+        });
+    };
 
     render() {
-        console.log(this.props.match.params.testValue);
+        console.log(this.state);
         return (
             <div className='videoPage'>
                 <div className='videoPlayer'>
                     <VideoPlayer videoId={this.props.match.params.id} />
                 </div>
-                <form className='videoForm'>
+                <form className='videoForm' onSubmit={this.handleSubmit}>
                     <div className='inputs'>
                         <label htmlFor="name">Name</label> <br />
-                        <input name='name' type="text" placeholder='name' />
+                        <input name='name' type="text" placeholder='name' onChange={this.handleInput} required />
                     </div>
                     <div className='inputs'>
                         <label htmlFor="comment">Comment</label> <br />
-                        <input name='comment' type="text" placeholder='...' />
+                        <input name='comment' type="text" placeholder='...' onChange={this.handleInput} required />
                     </div>
 
                     <button>Submit</button>
                 </form>
+
+                {
+                    <ul>
+                        {
+                            this.state.videInfo.map(el => {
+                                return (
+                                    <li>
+                                        <p className='username'>{el.name}</p>
+                                        <p>{el.comment}</p>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+
+                }
             </div>
         )
     }
